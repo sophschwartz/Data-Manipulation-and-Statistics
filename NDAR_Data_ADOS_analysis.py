2f0e40bda8bb45fb8682735a405943c2b3c25c0f
sophschwartz <54289524+sophschwartz@users.noreply.github.com>
2020-08-13 11:07:21 -0400

    Update NDAR_Data_ADOS_analysis.py

--- NDAR_Data_ADOS_analysis.py
+++ NDAR_Data_ADOS_analysis.py
@@ -19,8 +19,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn.svm import SVC
 """
 NDAR-Defined file names corresponding to each form:
     #2001 version of ADOS
@@ -103,7 +103,7 @@ def extract_variables(data_frame, version):
         col_coding_nocmt = [x for x in col_coding if not (x[-1].isalpha())]
         data_frame.rename(columns={
             'scoresumm_rangeofconcern': 'scoresumm_adosdiag'}, inplace=True)
-        col_diagnostic = ['scoresumm_rangeofconcern']
+        col_diagnostic = ['scoresumm_adosdiag']
 
     else:
         col_diagnostic = ['scoresumm_adosdiag']
@@ -150,87 +150,98 @@ def clean_data(data_frame, col_coding):
 
     # options for ados-2 classification should be autism, autism spectrum,
     # and nonspectrum
-    # df[scoresumm_adosdiag] = df[scoresumm_adosdiag].str.lower()
     df.scoresumm_adosdiag = df.scoresumm_adosdiag.str.lower()
 
-    autism_spellings = ['aurism', 'autsim', 'autim', 'austism', 'autisim',
-                        'austim']
-    for aut_string in autism_spellings:
-        df.scoresumm_adosdiag.replace({aut_string: 'autism'}, regex=True,
-                                      inplace=True)
-    spectrum_spellings = ['specturm', 'spectum', 'sepctrum']
-    for spec_string in spectrum_spellings:
-        df.scoresumm_adosdiag.replace({spec_string: 'spectrum'}, regex=True,
+    # nonspectrum alternative names
+    nonspectrum_options = ['language delay', '0', 'no', 'typ']
+    for string in nonspectrum_options:
+        df.loc[df['scoresumm_adosdiag'].str.contains(string),
+               'scoresumm_adosdiag'] = 'nonspectrum'
+
+    # find occurrances of mispellings and replace with the word autism
+    autism_misspelled = ['aurism', 'autsim', 'autim', 'austism', 'autisim',
+                         'austim']
+    for string in autism_misspelled:
+        df.scoresumm_adosdiag.replace({string: 'autism'}, regex=True,
                                       inplace=True)
 
-    df.scoresumm_adosdiag.replace(to_replace='autism spect',
-                                  value='autism spectrum', inplace=True)
-    df.scoresumm_adosdiag.replace(to_replace='aut', value='autism',
-                                  inplace=True)
-    df.scoresumm_adosdiag.replace(
-        to_replace='autism spectrum (mild but present )',
-        value='autism spectrum', inplace=True)
-    df.scoresumm_adosdiag.replace(to_replace='language delay',
-                                  value='nonspectrum', inplace=True)
-    df.scoresumm_adosdiag.replace({'autismspectrum': 'autism spectrum'},
-                                  regex=True, inplace=True)
-    df.loc[df['scoresumm_adosdiag'].str.contains('0'),
-           'scoresumm_adosdiag'] = 'nonspectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('no'),
-           'scoresumm_adosdiag'] = 'nonspectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('typ'),
-           'scoresumm_adosdiag'] = 'nonspectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('asd'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('1'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('autism spectrum disorder'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('autism-spectrum'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('autism  spectrum'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
+    # spectrum misspelled
+    spectrum_misspelled = ['specturm', 'spectum', 'sepctrum']
+    for string in spectrum_misspelled:
+        df.scoresumm_adosdiag.replace({string: 'spectrum'}, regex=True,
+                                      inplace=True)
+
+    # must just contain this item to be autism spectrum
+    autism_spectrum_options = ['asd', 'autism spectrum disorder', '1',
+                               'autism  spectrum', 'autism-spectrum',
+                               'autism spect', 'autism spectrum',
+                               'autismspectrum', 'moderate',
+                               'medium', 'pdd']
+    for string in autism_spectrum_options:
+        df.loc[df['scoresumm_adosdiag'].str.contains(string),
+               'scoresumm_adosdiag'] = 'autism spectrum'
+
+    # matches spectrum exactly, without non before it
     df.scoresumm_adosdiag.replace(to_replace='spectrum',
                                   value='autism spectrum', inplace=True)
-    df.loc[df['scoresumm_adosdiag'].str.contains('moderate'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('medium'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('pdd'),
-           'scoresumm_adosdiag'] = 'autism spectrum'
-    df.loc[df['scoresumm_adosdiag'].str.contains('2'),
-           'scoresumm_adosdiag'] = 'autism'
-    df.loc[df['scoresumm_adosdiag'].str.contains('high'),
-           'scoresumm_adosdiag'] = 'autism'
-    df.loc[df['scoresumm_adosdiag'].str.contains('autistic disorder'),
-           'scoresumm_adosdiag'] = 'autism'
 
-# deal with nan items
+    # replace autism alternatives with autism
+    autism_options = ['aut']
+    for string in autism_options:
+        df.scoresumm_adosdiag.replace(to_replace=string, value='autism',
+                                      inplace=True)
+
+    autism_options = ['2', 'high', 'autistic disorder']
+    for string in autism_options:
+        df.loc[df['scoresumm_adosdiag'].str.contains(string),
+               'scoresumm_adosdiag'] = 'autism spectrum'
+
+    # Concern items
+    df.scoresumm_adosdiag.replace(to_replace='little-to-no  concern',
+                                  value='little-to-no concern',
+                                  inplace=True)
+
+    # deal with nan items
     df.scoresumm_adosdiag.replace(to_replace='no diagnosis', value='',
                                   inplace=True)
-    df.loc[df['scoresumm_adosdiag'].str.contains('9'),
-           'scoresumm_adosdiag'] = ''
-    df.loc[df['scoresumm_adosdiag'].str.contains('n/a'),
-           'scoresumm_adosdiag'] = ''
-    df.loc[df['scoresumm_adosdiag'].str.contains('low'),
-           'scoresumm_adosdiag'] = ''
-    df.loc[df['scoresumm_adosdiag'].str.contains('3'),
-           'scoresumm_adosdiag'] = ''
+    nan_strings = ['9', 'n/a', 'low', '3', 'na']
+    for string in nan_strings:
+        df.loc[df['scoresumm_adosdiag'].str.contains(string),
+               'scoresumm_adosdiag'] = ''
     df.scoresumm_adosdiag.replace(to_replace='', value=np.nan,
                                   inplace=True)
-
-    # replace all 999 values with nan
-    df.replace(to_replace='999', value=np.nan, inplace=True)
-    # threshold for na drop currently set to 0
-    # df = df.dropna(axis=0, thresh=10)
     df = df.dropna()
 
-    # convert all rows besides subjectkey, sex, and scoresumm_adosdiag to int32
-    cols_to_conv = col_coding + ['interview_age']
-    df[cols_to_conv] = df[cols_to_conv].apply(pd.to_numeric, errors='coerce')
+    # replace all -9s with 9 for toddler version
+    # df[col_coding] = df[col_coding].abs()
+    # df2 = df.replace(to_replace=-9, value=9)
 
     return df
 
+
+# %%
+
+
+def format_datatypes(data_frame):
+    """
+    Format dataframe so values are all considered categorical.
+
+    Parameters
+    ----------
+    data_frame : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    data_frame2.
+
+    """
+    data_frame['interview_age'] = data_frame['interview_age'].astype('int32')
+    selected_cols = data_frame.select_dtypes(include=['object']).columns
+    data_frame[selected_cols] = data_frame[selected_cols].apply(
+        lambda x: x.astype('category'))
+    data_frame['subjectkey'] = data_frame['subjectkey'].astype('object')
+    return data_frame
 # %%
 
 
@@ -262,9 +273,40 @@ def descriptive_stats(data_frame, col_demo):
     sex_summ = data_frame.groupby('scoresumm_adosdiag')['sex'].value_counts()
     age_summ = data_frame.groupby('scoresumm_adosdiag')[
         'interview_age'].describe()
+    data_frame.groupby('scoresumm_adosdiag')['interview_age'].hist()
+    plt.xlim((0, 300))
+    plt.title('Histogram of Age and Group')
+    plt.xlabel('Age (months)')
+    plt.ylabel('Frequency')
 
     return age_summ, sex_summ
 
+# %%
+
+
+def one_hot_encoding(df, col_coding):
+    """
+    Conduct one-hot encoding of the categorical features.
+
+    Parameters
+    ----------
+    data_frame : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    x_cat = df[col_coding]
+    x_encoded = pd.get_dummies(x_cat)
+    df2 = df.drop(col_coding, axis=1)
+    df2.reset_index(drop=True, inplace=True)
+    x_encoded.reset_index(drop=True, inplace=True)
+
+    df3 = pd.concat([df2, x_encoded], axis=1)
+
+    return df3
 
 # %%
 
@@ -286,7 +328,8 @@ def split_dataset(data_frame, col_coding):
     y_test: pandas series object converted to a list of strings
 
     """
-    y = data_frame['scoresumm_adosdiag'].tolist()
+    #y = data_frame['scoresumm_adosdiag'].tolist()
+    y = data_frame['scoresumm_adosdiag']
     x = data_frame[col_coding]
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
     return x_train, x_test, y_train, y_test
@@ -309,6 +352,13 @@ def kfold_split(dataframe, col_coding):
     None.
 
     """
+    # evaluate model
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    n_scores = cross_val_score(
+        model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+    # report performance
+    print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+
     kf = KFold(n_splits=10, random_state=0)
     y_hat_all = []
     for train_index, test_index in kf.split(x, y):
@@ -319,30 +369,6 @@ def kfold_split(dataframe, col_coding):
         y_hat = clf.predict(X_test)
         y_hat_all.append(y_hat)
     return y_hat_all
-# %%
-
-
-def logistic_regression(data_frame):
-    """
-    Logistic regression to classify groups based on ADOS code data.
-
-    Use sklearn.linear_model.LogisticRegression
-    *** Figure out where LEARNING RATE gets defined in all of these functions
-    *** Also may be based on step-size or shrinkage
-    Parameters
-    ----------
-    data_frame : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    None.
-
-    """
-    from sklearn.linear_model import LogisticRegression
-    logreg = LogisticRegression()
-    scores_logreg, report_logreg = cross_validation(x, y, logreg)
-    print(scores)
 
 # %%
 
@@ -472,6 +498,9 @@ def svm(data_frame):
 
     Determine what items are most important in distinguishing diagnostic groups
     Use sklearn.svm, may or may not specify gamma and C arguments
+    C: float, default 1.0. Regularzation parameter
+    gamma: default = 'scale', 1
+    degree: integer, default is 3
 
     Parameters
     ----------
@@ -483,8 +512,81 @@ def svm(data_frame):
     None.
 
     """
-    clf = svm.SVC(gamma=0.001, C=100.)
+    clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+    clf.fit(X, y)
+    Pipeline(steps=[('standardscaler', StandardScaler()),
+                    ('svc', SVC(gamma='auto'))])
+    clf = SVC(gamma=0.001, C=100.)
 
+# %%
+
+
+# def logistic_regression(x_train, x_test, y_train, y_test):
+#     """
+#     Logistic regression to classify groups based on ADOS code data.
+#     Can't do because independent variables are not binary or continuous.
+
+#     Dimensionality reduction with PCA and LDA suggests regression would be
+#     done best on all variables, not reduced. Can also evaluate the same
+#     transform and model with different numbers of input features and choose
+#     the number of features (amount of dimensionality reduction) that results
+#     in the best average performance.
+
+
+#     Parameters
+#     ----------
+#     data_frame : TYPE
+#         DESCRIPTION.
+
+#     Returns
+#     -------
+#     None.
+
+#     """
+
+#     from sklearn.linear_model import LogisticRegression
+
+
+#     # logreg = LogisticRegression()
+#     # scores_logreg, report_logreg = cross_validation(x_train, y_train, logreg)
+#     # print(scores_logreg)
+
+#     logreg = LogisticRegression(C=1e5)
+#     logreg.fit(x_train, y_train)
+#     y_pred=logreg.predict(x_test)
+#     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+#     #print("Precision:", metrics.precision_score(y_test, y_pred))
+#     #print("Recall:", metrics.recall_score(y_test, y_pred))
+
+
+#     x_min, x_max = x_train[:, 0].min() - .5, x_train[:, 0].max() + .5
+#     y_min, y_max = x_train[:, 1].min() - .5, x_train[:, 1].max() + .5
+#     h = .02  # step size in the mesh
+#     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+#     Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+
+#     # Put the result into a color plot
+#     Z = Z.reshape(xx.shape)
+#     plt.figure(1, figsize=(4, 3))
+#     plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+#     # Plot also the training points
+#     plt.scatter(x_train[:, 0], x_train[:, 1], c=y_train, edgecolors='k', cmap=plt.cm.Paired)
+#     plt.xlabel('Sepal length')
+#     plt.ylabel('Sepal width')
+
+#     plt.xlim(xx.min(), xx.max())
+#     plt.ylim(yy.min(), yy.max())
+#     plt.xticks(())
+#     plt.yticks(())
+
+#     plt.show()
+
+#     predictions = logreg.predict(x_test)
+#     score = logreg.score(x_test, y_test)
+#     print(score)
+#     cm = metrics.confusion_matrix(y_test, predictions)
+#     print(cm)
 # %%
 
 
@@ -508,7 +610,7 @@ def forest(data_frame):
     classifier = RandomForestClassifier(max_depth=2, random_state=0)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
-    
+
     rfc = RandomForestClassifier(n_estimators=10, max_depth=None,
                                  min_samples_split=2, random_state=0)
     scores_rfc, report_rfc = cross_validation(x, y, rfc)
@@ -558,6 +660,15 @@ def SGD(data_frame):
     clf = SGDClassifier(loss="log", penalty="l2", max_iter=5)
 
 # %%
+
+# evaluate a given model using cross-validation
+
+
+def evaluate_model(model, X, y):
+    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    scores = cross_val_score(
+        model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+    return scores
 
 
 def cross_validation(x_train, y_train, x_test, y_test, clf):
@@ -615,7 +726,11 @@ def main():
     data_frame1 = read_tables(data_folder, file_name)
     data_frame2, col_demo, col_coding, col_diag = extract_variables(
         data_frame1, 'toddler')
-
+    data_frame3 = clean_data(data_frame2, col_coding)
+    age_summ_t, sex_summ_t = descriptive_stats(data_frame3, col_coding)
+    x_train, x_test, y_train, y_test = split_dataset(data_frame3, col_coding)
+    pca_results = pca(x_train, x_test, y_train, y_test)
+    lda_results = lda(x_train, x_test, y_train, y_test)
 
     # Module 1
     file_name = "ados1_201201.txt"
@@ -623,23 +738,59 @@ def main():
     data_frame2, col_demo, col_coding, col_diag = extract_variables(
         data_frame1, 'not toddler')
     data_frame3 = clean_data(data_frame2, col_coding)
-    age_summ_1, sex_summ_1 = descriptive_stats(data_frame3)
+    age_summ_1, sex_summ_1 = descriptive_stats(data_frame3, col_demo)
     x_train, x_test, y_train, y_test = split_dataset(data_frame3, col_coding)
     pca_results = pca(x_train, x_test, y_train, y_test)
     lda_results = lda(x_train, x_test, y_train, y_test)
-    
 
     # Module 2
     file_name = "ados2_201201.txt"
     data_frame1 = read_tables(data_folder, file_name)
     data_frame2, col_demo, col_coding, col_diag = extract_variables(
         data_frame1, 'not toddler')
+    data_frame3 = clean_data(data_frame2, col_coding)
+    data_frame4 = format_datatypes(data_frame3)
+    age_summ_2, sex_summ_2 = descriptive_stats(data_frame4, col_coding)
+    x_train, x_test, y_train, y_test = split_dataset(data_frame4, col_coding)
+    pca_results = pca(x_train, x_test, y_train, y_test)
+    lda_results = lda(x_train, x_test, y_train, y_test)
 
     # Module 3
     file_name = "ados3_201201.txt"  # 4699 usable entries
     data_frame1 = read_tables(data_folder, file_name)
     data_frame2, col_demo, col_coding, col_diag = extract_variables(
         data_frame1, 'not toddler')
-
+    data_frame3 = clean_data(data_frame2, col_coding)
+    age_summ_3, sex_summ_3 = descriptive_stats(data_frame3, col_coding)
+    x_train, x_test, y_train, y_test = split_dataset(data_frame3, col_coding)
+    pca_results = pca(x_train, x_test, y_train, y_test)
+    lda_results = lda(x_train, x_test, y_train, y_test)
 
     return()
+
+# get a list of models to evaluate
+
+
+def get_models():
+    models = dict()
+    for i in range(1, 21):
+        steps = [('pca', PCA(n_components=i)), ('m', LogisticRegression())]
+        models[str(i)] = Pipeline(steps=steps)
+    return models
+
+
+# define dataset
+X, y = get_dataset()
+# get the models to evaluate
+models = get_models()
+# evaluate the models and store results
+results, names = list(), list()
+for name, model in models.items():
+    scores = evaluate_model(model, X, y)
+    results.append(scores)
+    names.append(name)
+    print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+# plot model performance for comparison
+pyplot.boxplot(results, labels=names, showmeans=True)
+pyplot.xticks(rotation=45)
+pyplot.show()
